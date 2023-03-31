09e0d6e11c03dbe1aba480a3ab0f2fecbb05e9f0
Ignacio Rivas <rivasign@gmail.com>
2023-03-31 10:39:09 +0200

    Small ux tweaks

--- x-pack/plugins/remote_clusters/public/application/sections/remote_cluster_add/remote_cluster_add.js
+++ x-pack/plugins/remote_clusters/public/application/sections/remote_cluster_add/remote_cluster_add.js
@@ -7,16 +7,28 @@
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
   EuiPageBody,
   EuiPageContentBody_Deprecated as EuiPageContentBody,
+  EuiStepsHorizontal,
+  EuiSpacer,
+  EuiCard,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiText,
+  EuiCallOut,
+  EuiButton,
 } from '@elastic/eui';
 
 import { extractQueryParams } from '../../../shared_imports';
 import { getRouter, redirect } from '../../services';
 import { setBreadcrumbs } from '../../services/breadcrumb';
 import { RemoteClusterPageTitle, RemoteClusterForm } from '../components';
+
+const CREATE_CONNECTION = 0;
+const AUTH_METHOD = 1;
 
 export class RemoteClusterAdd extends PureComponent {
   static propTypes = {
@@ -25,6 +37,14 @@ export class RemoteClusterAdd extends PureComponent {
     addClusterError: PropTypes.object,
     clearAddClusterErrors: PropTypes.func,
   };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      currentStep: AUTH_METHOD,
+    };
+  }
 
   componentDidMount() {
     setBreadcrumbs('add');
@@ -35,8 +55,32 @@ export class RemoteClusterAdd extends PureComponent {
     this.props.clearAddClusterErrors();
   }
 
+  getStepDefinitions = () => {
+    const { currentStep } = this.state
+
+    return [
+      {
+        step: CREATE_CONNECTION,
+        title: i18n.translate('xpack.remoteClusters.clusterWizard.createConnectionLabel', {
+          defaultMessage: 'Create Connection',
+        }),
+        status: currentStep === CREATE_CONNECTION ? 'current' : 'complete',
+        onClick: () => {},
+      },
+      {
+        step: AUTH_METHOD,
+        title: i18n.translate('xpack.remoteClusters.clusterWizard.authMethodLabel', {
+          defaultMessage: 'Authentication method',
+        }),
+        status: currentStep === AUTH_METHOD ? 'current' : 'incomplete',
+        onClick: () => {},
+      },
+    ]
+  }
+
   save = (clusterConfig) => {
-    this.props.addCluster(clusterConfig);
+    this.setState({ currentStep: 1 })
+    // this.props.addCluster(clusterConfig);
   };
 
   cancel = () => {
@@ -58,6 +102,7 @@ export class RemoteClusterAdd extends PureComponent {
 
   render() {
     const { isAddingCluster, addClusterError } = this.props;
+    const { currentStep } = this.state;
 
     return (
       <EuiPageBody restrictWidth={true} data-test-subj="add-remote-cluster">
@@ -77,12 +122,69 @@ export class RemoteClusterAdd extends PureComponent {
             }
           />
 
-          <RemoteClusterForm
-            isSaving={isAddingCluster}
-            saveError={addClusterError}
-            save={this.save}
-            cancel={this.cancel}
-          />
+          <EuiStepsHorizontal steps={this.getStepDefinitions()} />
+          <EuiSpacer size="m" />
+
+          {currentStep === CREATE_CONNECTION && (
+            <RemoteClusterForm
+              isSaving={isAddingCluster}
+              saveError={addClusterError}
+              save={this.save}
+              cancel={this.cancel}
+            />
+          )}
+
+          {currentStep === AUTH_METHOD && (
+            <>
+              <EuiText grow={false} size="s">
+                <p>Your cluster has now been created and its time to configure its authentication method.</p>
+              </EuiText>
+              <EuiSpacer size="xxl" />
+              <EuiFlexGroup>
+                <EuiFlexItem>
+                  <EuiCard
+                    layout="horizontal"
+                    title="With API keys"
+                    betaBadgeProps={{ label: 'Recommended', color: 'accent' }}
+                  >
+                    <EuiCallOut color="warning" iconType="warning">
+                      <p>
+                        Remote clusters configured to use API key authentication will not work with CCR.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      </p>
+                    </EuiCallOut>
+                    <EuiSpacer size="l" />
+
+                    <EuiText grow={false} size="s">
+                      <p>This is the preffered way of setting up authentication for connecting to Remote Clusters. This is more secure, flexible and powerful than the certificates based one.</p>
+                    </EuiText>
+
+                    <EuiFlexGroup justifyContent="flexEnd">
+                      <EuiFlexItem grow={false}>
+                        <EuiButton>Documentation</EuiButton>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiCard>
+                </EuiFlexItem>
+
+                <EuiFlexItem>
+                  <EuiCard
+                    layout="horizontal"
+                    title="With certificates"
+                  >
+                    <EuiText grow={false} size="s">
+                      <p>This is our easier to setup method, but less secure than the API key based approach.</p>
+                    </EuiText>
+                    <EuiFlexGroup justifyContent="flexEnd">
+                      <EuiFlexItem grow={false}>
+                        <EuiButton>Documentation</EuiButton>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiCard>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </>
+          )}
         </EuiPageContentBody>
       </EuiPageBody>
     );
